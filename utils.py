cecb94d75b496adf817d881601b81cc8e19840b7
Aditya2311-del <dharpaladitya@gmail.com>
2025-06-11 21:46:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_ta as ta
 
-def get_data():
+def get_data(assets):
    tv=TvDatafeed()
-   assets=['RELIANCE','TCS','HDFCBANK','ICICIBANK','BHARTIARTL']
    data_dict = {}
-
    for asset in assets:
       data = tv.get_hist(symbol=asset, exchange='NSE', interval=Interval.in_daily, n_bars=500)
       data_dict[asset] = data
